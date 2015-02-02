27aaf639f30af5ddf7065627108148805cf91f27
Ulysses Popple <ulysses.popple@gmail.com>
2015-02-02 16:00:48 -0500

    Start on PlayerView

--- Views/PlayerView.js
+++ Views/PlayerView.js
@@ -1,5 +1,6 @@
 var PlayerView = (function () {
     function PlayerView() {
+        this.content = $("<div>", { class: "controls" });
     }
     PlayerView.prototype.render = function (el) {
         var mic = $("<a>", {
@@ -14,7 +15,17 @@ var PlayerView = (function () {
             e.preventDefault();
             console.log("Start mic");
         });
-        $(el).append(mic);
+        var soundcloud = $('<div>', { class: 'soundcloud', text: 'Soundcloud URL:' });
+        var input = $("<input>", { class: 'soundcloud-input', type: 'text' });
+        input.change(function () {
+            console.log(input.val());
+        });
+        soundcloud.append(input);
+        var audioPlayer = $("<audio />", { class: 'audio-player', controls: true });
+        this.content.append(mic);
+        this.content.append(soundcloud);
+        this.content.append(audioPlayer);
+        $(el).append(this.content);
     };
     return PlayerView;
 })();
--- Views/PlayerView.ts
+++ Views/PlayerView.ts
@@ -1,4 +1,9 @@
 class PlayerView implements IControllerView {
+  private content: JQuery;
+  constructor() {
+    this.content = $("<div>", { class:"controls" });
+  }
+
   render(el: HTMLElement): void {
 
     var mic: JQuery = $("<a>", {
@@ -17,6 +22,23 @@ class PlayerView implements IControllerView {
       console.log("Start mic");
     });
 
-    $(el).append(mic);
+    var soundcloud =
+      $('<div>', { class: 'soundcloud', text: 'Soundcloud URL:' });
+
+    var input = $("<input>", { class: 'soundcloud-input', type: 'text' });
+
+    input.change(function() {
+      console.log(input.val())
+    });
+
+    soundcloud.append(input);
+
+    var audioPlayer = $("<audio />", {class: 'audio-player', controls: true});
+
+    this.content.append(mic);
+    this.content.append(soundcloud);
+    this.content.append(audioPlayer);
+
+    $(el).append(this.content);
   }
 }
